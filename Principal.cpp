cc8f5ea8486be16e439ada5985ae52754cd649d3
carlos bucay <carlos.deberes@hotmail.com>
2020-05-30 23:03:05 -0500

    segundo commit declaracion de variables

--- Principal.cpp
+++ Principal.cpp
@@ -2,14 +2,17 @@
 int fibonaci(int n)
 {
 // ingresar codigo
+int a=0, b=1;
 return 0;
 }
 int factorial (int p)
 {
 // ingresar codigo
+int factorial =1;
 return 0;
 }
 main ()
 {
 // funcion principal
+int numero;
 }
